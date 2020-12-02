3801cf4529623178193d68da3b9d24d25014f192
sourceCodeISA <isabel.gogov1@libero.it>
2020-12-02 17:32:13 +0100

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-const apiFilms = "https://api.themoviedb.org/3/search/movie?api_key=bbed237dc6562aa068a11a0bbcb33c30&query=ritorno+al+futuro&language=it-IT"
 
 var app = new Vue({
   el: '#app',
   data: {
+    searchFilm: "",
     films: []
   },
-  mounted: function () {
-    axios.get(apiFilms)
-      .then(risposta => {
 
-        // this.films = risposta.data;
-        // console.log(this.films);
-      });
+  methods: {
+    searchClick: function () {
+      let apiFilms = "https://api.themoviedb.org/3/search/movie?api_key=bbed237dc6562aa068a11a0bbcb33c30&language=it-IT&query=" + this.searchFilm;
 
+      if (this.searchFilm !== "") { // evita il click vuoto
+
+        axios.get(apiFilms)
+          .then(risposta => {
+
+            this.films = risposta.data.results;
+            console.log(this.films);
+          });
+      }
+
+    }
   }
 
 });
+
